7e60a31659b613cc5021bf7da9758a04ee96b521
yusufyilmazmrf <yusuf.yilmaz@marfeel.com>
2020-01-28 18:11:27 +0100

    commit

---
+++ components/figma.story.jsx
@@ -0,0 +1,10 @@
+
+
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { FigmaFrame } from '../../storybook-addon-figma/dist/index.js';
+
+// storiesOf('Examples|Figma', module)
+// 	.add('Embed file', () => {
+// 		return <FigmaFrame src="https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File">hi</FigmaFrame>;
+// 	});
